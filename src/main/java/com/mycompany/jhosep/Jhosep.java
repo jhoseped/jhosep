2facf665929c84526b3775156e2610703ff021b0
Jhosep <Jhosep@QP-LT156R5>
2023-09-04 22:49:07 -0500

    se esta enviando

---
+++ src/main/java/com/mycompany/jhosep/Jhosep.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jhosep;
+
+/**
+ *
+ * @author Jhosep
+ */
+public class Jhosep {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
